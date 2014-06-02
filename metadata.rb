54bcdb5713958343b63bb832481bc565a44bfe8e
Roshan Singh <roshan@opsline.com>
2014-06-02 13:09:42 -0400

    updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'opsline-chef-backup'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Opsline, LLC'
+maintainer_email 'roshan@opsline.com'
 license          'All rights reserved'
-description      'Installs/Configures opsline-chef-backup'
+description      'Installs/Configures chef server backup and restore scripts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
